e923610c2775c23843842f53f9915d0ba539f467
Jefferson Chavez Diaz <jefersonchavezdiaz.14@hotmail.com>
2024-06-19 15:45:09 -0500

    curso de node js con midudev, módulos

---
+++ cjs/index.js
@@ -0,0 +1,5 @@
+//Importación clásica de los módulos (Antigua) <- Sistema de Módulos CommonJS
+
+const { sum } = require('./sum')
+
+console.log(sum(1, 2))
---
+++ cjs/sum.js
@@ -0,0 +1,9 @@
+function sum(a, b) {
+  return a + b
+}
+
+//Exportación clásica de los módulos (Antigua) <- Sistema de Módulos CommonJS
+
+module.exports = {
+  sum,
+}
