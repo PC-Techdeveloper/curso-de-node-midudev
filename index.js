d3e26eb1b35b21e8fcd687d6a1cda015ed3382eb
Jefferson Chavez Diaz <jefersonchavezdiaz.14@hotmail.com>
2024-06-19 16:17:54 -0500

    add uptime in the projects

--- index.js
+++ index.js
@@ -16,3 +16,4 @@ console.log('Arquitectura del sistema operativo: ', os.arch())
 console.log('CPUs del sistema operativo: ', os.cpus().length) // <- Escalar procesos en Node
 console.log('Memoria disponible del sistema operativo: ', os.freemem() / 1024 / 1024)
 console.log('Memoria total del sistema operativo: ', os.totalmem() / 1024 / 1024)
+console.log('uptime del sistema operativo: ', os.uptime() / 60 / 60)
