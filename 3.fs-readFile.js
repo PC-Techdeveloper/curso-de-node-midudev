580a43120af44b23f1ab0c0fc60eab3aadb5bce8
Jefferson Chavez Diaz <136255274+PC-Techdeveloper@users.noreply.github.com>
2024-06-30 20:51:37 -0500

    node with async (readFile())

--- 3.fs-readFile.js
+++ 3.fs-readFile.js
@@ -1,8 +1,18 @@
+// Síncrono -> readFileSync (⚠️ Solo para lectura)
+//Leer un archivo
+
+// Async -> readFile -> Callback (Funciones que se ejecutan cuando una tarea ha terminado)
+
 const fs = require('node:fs')
 
-// Síncrono
+console.log('Leyendo el primer archivo...')
+fs.readFile('./archivo.txt', 'utf-8', (err, text) => { // <- Ejecuta este callback
+  console.log('primer texto:', text)
+})
 
-//Leer un archivo
-const text = fs.readFileSync('./archivo.txt', 'utf8')
+console.log('--> Hacer cosas mientras lee el archivo...')
 
-console.log(text)
+console.log('Leyendo el segundo archivo...')
+fs.readFile('./archivo2.txt', 'utf-8', (err, text) => {
+  console.log('segundo texto:', text)
+})
