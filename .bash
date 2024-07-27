a61bbc94f9a9cbbf9d1630b77e6d19baccd09f86
JEFFERSON CHAVEZ DIAZ <136255274+PC-Techdeveloper@users.noreply.github.com>
2024-07-27 09:28:28 -0500

    review files node js

---
+++ .bash
@@ -0,0 +1,1 @@
+oh-my-posh init pwsh --config 'C:/Users/Posh/craver.omp.json' | Invoke-Expression
--- 4.fs-promises.mjs
+++ 4.fs-promises.mjs
@@ -3,19 +3,19 @@
 // const { promisify } = require('node:util') // With case natives
 // //Create version of promisify that returns a promise
 // const readFilePromise = promisify(fs.readFile)
-const fs = require('node:fs/promises')
+const fs = require('node:fs/promises');
 
 //Leer un archivo
-console.log('Leyendo el primer archivo...')
+console.log('Leyendo el primer archivo...');
 
 // Async -> readFile -> Callback (Funciones que se ejecutan cuando una tarea ha terminado)
-fs.readFile('./archivo.txt', 'utf-8')
-  .then((text) => console.log('Primer texto:', text))
+fs.readFile('./archivo.txt', 'utf-8').then(text =>
+  console.log('Primer texto:', text)
+);
 
-console.log(' --> Hacer cosas mientras lee el archivo...')
-console.log('Leyendo el segundo archivo...')
+console.log(' --> Hacer cosas mientras lee el archivo...');
+console.log('Leyendo el segundo archivo...');
 
-fs.readFile('./archivo2.txt', 'utf-8')
-  .then((secondText) => {
-  console.log('Segundo texto:', secondText)
-})
+fs.readFile('./archivo2.txt', 'utf-8').then(secondText => {
+  console.log('Segundo texto:', secondText);
+});
